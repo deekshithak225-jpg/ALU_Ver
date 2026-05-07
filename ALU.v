468372495cb993c7484b88dd719c553c76fb6fc7
deekshithak225-jpg <deekshitha.k225@gmail.com>
2026-05-07 15:05:44 +0530

    modified alu project

--- ALU.v
+++ ALU.v
@@ -1,4 +1,4 @@
-lu(INP_VALID,OPA,OPB,CIN,CLK,RST,CMD,CE,MODE,COUT,OFLOW,RES,G,E,L,ERR);
+module alu(INP_VALID,OPA,OPB,CIN,CLK,RST,CMD,CE,MODE,COUT,OFLOW,RES,G,E,L,ERR);
 parameter N=8;
 
   input [1:0]INP_VALID;
@@ -14,113 +14,92 @@ parameter N=8;
   output reg ERR ;
   reg signed [N-1:0]s_OPA;
   reg signed [N-1:0]s_OPB;
-
-  wire [N-1:0] SUM;
+  wire [N-1:0]SUM;
   assign SUM=s_OPA+s_OPB;
-  wire [N-1:0] DIFF;
+  wire [N-1:0]DIFF;
   assign DIFF=s_OPA-s_OPB;
-
-
-  reg [N-1:0]   pipe1_OPA_9,  pipe1_OPB_9;
-  reg [N-1:0]   pipe1_OPA_10, pipe1_OPB_10;
-  reg           pipe1_valid_9, pipe1_valid_10;
-
-
-  reg [2*N-1:0] pipe2_product_9, pipe2_product_10;
-  reg           pipe2_valid_9,   pipe2_valid_10;
-
-
-  reg [2*N-1:0] pipe3_product_9, pipe3_product_10;
-  reg           pipe3_valid_9,   pipe3_valid_10;
-
+  reg [N-1:0]pipe1_OPA_9,pipe1_OPB_9;
+  reg [N-1:0]pipe1_OPA_10,pipe1_OPB_10;
+  reg pipe1_valid_9, pipe1_valid_10;
+  reg [2*N-1:0] pipe2_product_9,pipe2_product_10;
+  reg pipe2_valid_9,pipe2_valid_10;
+  reg [2*N-1:0] pipe3_product_9,pipe3_product_10;
+  reg pipe3_valid_9,pipe3_valid_10;
   always @(*)
   begin
-    s_OPA = OPA;
-    s_OPB = OPB;
+    s_OPA=OPA;
+    s_OPB=OPB;
   end
-
- 
-  always @(posedge CLK or posedge RST)
-  begin
-    if (RST)
-    begin
-      pipe1_OPA_9    <= 0; pipe1_OPB_9    <= 0; pipe1_valid_9  <= 0;
-      pipe1_OPA_10   <= 0; pipe1_OPB_10   <= 0; pipe1_valid_10 <= 0;
-      pipe2_product_9  <= 0; pipe2_valid_9  <= 0;
-      pipe2_product_10 <= 0; pipe2_valid_10 <= 0;
-      pipe3_product_9  <= 0; pipe3_valid_9  <= 0;
-      pipe3_product_10 <= 0; pipe3_valid_10 <= 0;
-    end
-    else if (CE)
-    begin
-
-     
-      if (MODE && (CMD == 4'b1001) && (INP_VALID == 2'b11))
-      begin
-        pipe1_OPA_9   <= OPA;
-        pipe1_OPB_9   <= OPB;
-        pipe1_valid_9 <= 1'b1;
-      end
-      else
-        pipe1_valid_9 <= 1'b0;
-
-      if (MODE && (CMD == 4'b1010) && (INP_VALID == 2'b11))
-      begin
-        pipe1_OPA_10   <= OPA;
-        pipe1_OPB_10   <= OPB;
-        pipe1_valid_10 <= 1'b1;
-      end
-      else
-        pipe1_valid_10 <= 1'b0;
-
-    
-      if (pipe1_valid_9)
-      begin
-        pipe2_product_9 <= pipe1_OPA_9 * pipe1_OPB_9;
-        pipe2_valid_9   <= 1'b1;
-      end
-      else
-        pipe2_valid_9 <= 1'b0;
-
-      if (pipe1_valid_10)
-      begin
-        pipe2_product_10 <= (pipe1_OPA_10 << 1) * pipe1_OPB_10;
-        pipe2_valid_10   <= 1'b1;
-      end
-      else
-        pipe2_valid_10 <= 1'b0;
-
-    
-      if (pipe2_valid_9)
-      begin
-        pipe3_product_9 <= pipe2_product_9;
-        pipe3_valid_9   <= 1'b1;
-      end
-      else
-        pipe3_valid_9 <= 1'b0;
-
-      if (pipe2_valid_10)
-      begin
-        pipe3_product_10 <= pipe2_product_10;
-        pipe3_valid_10   <= 1'b1;
-      end
-      else
-        pipe3_valid_10 <= 1'b0;
-
-    end
-  end
-
   always@(posedge CLK or posedge RST)
   begin
     if(RST)
     begin
-      RES   <= 0;
-      COUT  <= 1'b0;
-      OFLOW <= 1'b0;
-      G     <= 1'b0;
-      E     <= 1'b0;
-      L     <= 1'b0;
-      ERR   <= 1'b0;
+      pipe1_OPA_9<=0;pipe1_OPB_9<=0;pipe1_valid_9<= 0;
+      pipe1_OPA_10<=0;pipe1_OPB_10 <=0;pipe1_valid_10<=0;
+      pipe2_product_9<=0;pipe2_valid_9<=0;
+      pipe2_product_10<=0;pipe2_valid_10<=0;
+      pipe3_product_9<=0;pipe3_valid_9<=0;
+      pipe3_product_10<=0;pipe3_valid_10<=0;
+    end
+    else if (CE)
+    begin
+      if(MODE && (CMD==4'b1001)&&(INP_VALID==2'b11))
+      begin
+        pipe1_OPA_9<=OPA;
+        pipe1_OPB_9<=OPB;
+        pipe1_valid_9<=1'b1;
+      end
+      else
+        pipe1_valid_9<=1'b0;
+      if (MODE && (CMD==4'b1010) && (INP_VALID==2'b11))
+      begin
+        pipe1_OPA_10<=OPA;
+        pipe1_OPB_10<=OPB;
+        pipe1_valid_10<=1'b1;
+      end
+      else
+        pipe1_valid_10<=1'b0;
+      if(pipe1_valid_9)
+      begin
+        pipe2_product_9<=pipe1_OPA_9*pipe1_OPB_9;
+        pipe2_valid_9<=1'b1;
+      end
+      else
+        pipe2_valid_9<=1'b0;
+      if(pipe1_valid_10)
+      begin
+        pipe2_product_10<=(pipe1_OPA_10<< 1)*pipe1_OPB_10;
+        pipe2_valid_10<=1'b1;
+      end
+      else
+        pipe2_valid_10<=1'b0;
+      if(pipe2_valid_9)
+      begin
+        pipe3_product_9<=pipe2_product_9;
+        pipe3_valid_9<=1'b1;
+      end
+      else
+        pipe3_valid_9<=1'b0;
+      if(pipe2_valid_10)
+      begin
+        pipe3_product_10<=pipe2_product_10;
+        pipe3_valid_10<=1'b1;
+      end
+      else
+        pipe3_valid_10<=1'b0;
+    end
+  end
+  always@(posedge CLK or posedge RST)
+  begin
+    if(RST)
+    begin
+      RES<=0;
+      COUT<=1'b0;
+      OFLOW<=1'b0;
+      G<=1'b0;
+      E<=1'b0;
+      L<=1'b0;
+      ERR<=1'b0;
     end
     else if(CE)
     begin
@@ -138,8 +117,8 @@ parameter N=8;
           begin
             if(INP_VALID==2'b11)
             begin
-              OFLOW <= (OPA<OPB) ? 1 : 0;
-              RES   <= OPA-OPB;
+              OFLOW<= (OPA<OPB) ? 1 : 0;
+              RES<= OPA-OPB;
             end
             else
               ERR <= 1'b1;
@@ -149,38 +128,38 @@ parameter N=8;
             if(INP_VALID==2'b11)
               {COUT,RES} <= OPA+OPB+CIN;
             else
-              ERR <= 1'b1;
+              ERR<= 1'b1;
           end
           4'b0011:
           begin
             if(INP_VALID==2'b11)
             begin
-              OFLOW <= (OPA<OPB) ? 1 : 0;
-              RES   <= OPA-OPB-CIN;
+              OFLOW<= (OPA<OPB) ? 1 : 0;
+              RES<= OPA-OPB-CIN;
             end
             else
-              ERR <= 1'b1;
+              ERR<= 1'b1;
           end
           4'b0100:
           begin
             if(INP_VALID==2'b01)
-              RES <= OPA+1;
+              RES<= OPA+1;
             else
-              ERR <= 1'b1;
+              ERR<= 1'b1;
           end
           4'b0101:
           begin
             if(INP_VALID==2'b01)
-              RES <= OPA-1;
+              RES<= OPA-1;
             else
-              ERR <= 1'b1;
+              ERR<= 1'b1;
           end
           4'b0110:
           begin
             if(INP_VALID==2'b10)
-              RES <= OPB+1;
+              RES<= OPB+1;
             else
-              ERR <= 1'b1;
+              ERR<= 1'b1;
           end
           4'b0111:
           begin
@@ -240,77 +219,77 @@ parameter N=8;
           begin
             if(INP_VALID==2'b11)
             begin
-              RES   <= s_OPA+s_OPB;
-              OFLOW <= (s_OPA[N-1]==s_OPB[N-1]) && ((SUM[N-1])!=s_OPA[N-1]);
-              E     <= (s_OPA==s_OPB);
-              G     <= (s_OPA>s_OPB);
-              L     <= (s_OPA<s_OPB);
+              RES<= s_OPA+s_OPB;
+              OFLOW<= (s_OPA[N-1]==s_OPB[N-1]) && ((SUM[N-1])!=s_OPA[N-1]);
+              E<= (s_OPA==s_OPB);
+              G<= (s_OPA>s_OPB);
+              L<= (s_OPA<s_OPB);
             end
             else
-              ERR <= 1'b1;
+              ERR<= 1'b1;
           end
           4'b1100:
           begin
             if(INP_VALID==2'b11)
             begin
-              RES   <= s_OPA-s_OPB;
-              OFLOW <= (s_OPA[N-1]!=s_OPB[N-1]) && ((DIFF[N-1])!=s_OPA[N-1]);
-              E     <= (s_OPA==s_OPB);
-              G     <= (s_OPA>s_OPB);
-              L     <= (s_OPA<s_OPB);
+              RES<=s_OPA-s_OPB;
+              OFLOW<=(s_OPA[N-1]!=s_OPB[N-1]) && ((DIFF[N-1])!=s_OPA[N-1]);
+              E<=(s_OPA==s_OPB);
+              G<=(s_OPA>s_OPB);
+              L<=(s_OPA<s_OPB);
             end
             else
               ERR <= 1'b1;
           end
           default:
           begin
-            RES   <= 0;
-            COUT  <= 1'b0;
-            OFLOW <= 1'b0;
-            G     <= 1'b0;
-            E     <= 1'b0;
-            L     <= 1'b0;
-            ERR   <= 1'b0;
+            RES<=0;
+            COUT<=1'b0;
+            OFLOW<=1'b0;
+            G<=1'b0;
+            E<=1'b0;
+            L<=1'b0;
+            ERR<=1'b0;
           end
         endcase
       end
       else
       begin
-        RES   <= 0;
-        COUT  <= 1'b0;
-        OFLOW <= 1'b0;
-        G     <= 1'b0;
-        E     <= 1'b0;
-        L     <= 1'b0;
-        ERR   <= 1'b0;
+        RES<=0;
+        COUT<=1'b0;
+        OFLOW<=1'b0;
+        G<=1'b0;
+        E<=1'b0;
+        L<=1'b0;
+        ERR<=1'b0;
         case(CMD)
           4'b0000:
           begin
             if(INP_VALID==2'b11)
-              RES <= OPA&OPB;
+              RES<=OPA&OPB;
             else
-              ERR <= 1'b1;
+              ERR<=1'b1;
           end
           4'b0001:
           begin
             if(INP_VALID==2'b11)
-              RES <= ~(OPA&OPB);
+              RES<=~(OPA&OPB);
             else
-              ERR <= 1'b1;
+              ERR<=1'b1;
           end
           4'b0010:
           begin
             if(INP_VALID==2'b11)
-              RES <= OPA|OPB;
+              RES<=OPA|OPB;
             else
-              ERR <= 1'b1;
+              ERR<=1'b1;
           end
           4'b0011:
           begin
             if(INP_VALID==2'b11)
-              RES <= ~(OPA|OPB);
+              RES<=~(OPA|OPB);
             else
-              ERR <= 1'b1;
+              ERR<=1'b1;
           end
           4'b0100:
           begin
@@ -373,14 +352,14 @@ parameter N=8;
             if(INP_VALID==2'b11)
             begin
               casex(OPB[7:0])
-                8'b0000_x000: RES <= OPA;
-                8'b0000_x001: RES <= {OPA[N-2:0],OPA[N-1]};
-                8'b0000_x010: RES <= {OPA[N-3:0],OPA[N-1:N-2]};
-                8'b0000_x011: RES <= {OPA[N-4:0],OPA[N-1:N-3]};
-                8'b0000_x100: RES <= {OPA[N-5:0],OPA[N-1:N-4]};
-                8'b0000_x101: RES <= {OPA[N-6:0],OPA[N-1:N-5]};
-                8'b0000_x110: RES <= {OPA[N-7:0],OPA[N-1:N-6]};
-                8'b0000_x111: RES <= {OPA[N-8:0],OPA[N-1:N-7]};
+                8'b0000_x000:RES<=OPA;
+                8'b0000_x001:RES<={OPA[N-2:0],OPA[N-1]};
+                8'b0000_x010:RES<={OPA[N-3:0],OPA[N-1:N-2]};
+                8'b0000_x011:RES<={OPA[N-4:0],OPA[N-1:N-3]};
+                8'b0000_x100:RES<={OPA[N-5:0],OPA[N-1:N-4]};
+                8'b0000_x101:RES<={OPA[N-6:0],OPA[N-1:N-5]};
+                8'b0000_x110:RES<={OPA[N-7:0],OPA[N-1:N-6]};
+                8'b0000_x111:RES<={OPA[N-8:0],OPA[N-1:N-7]};
                 default:;
               endcase
               ERR <= 1'b0;
@@ -395,14 +374,14 @@ parameter N=8;
             if(INP_VALID==2'b11)
             begin
               casex(OPB[7:0])
-                8'b0000_x000: RES <= OPA;
-                8'b0000_x001: RES <= {OPA[0],OPA[N-1:1]};
-                8'b0000_x010: RES <= {OPA[1:0],OPA[N-1:2]};
-                8'b0000_x011: RES <= {OPA[2:0],OPA[N-1:3]};
-                8'b0000_x100: RES <= {OPA[3:0],OPA[N-1:4]};
-                8'b0000_x101: RES <= {OPA[4:0],OPA[N-1:5]};
-                8'b0000_x110: RES <= {OPA[5:0],OPA[N-1:6]};
-                8'b0000_x111: RES <= {OPA[6:0],OPA[N-1:7]};
+                8'b0000_x000:RES<=OPA;
+                8'b0000_x001:RES<={OPA[0],OPA[N-1:1]};
+                8'b0000_x010:RES<={OPA[1:0],OPA[N-1:2]};
+                8'b0000_x011:RES<={OPA[2:0],OPA[N-1:3]};
+                8'b0000_x100:RES<={OPA[3:0],OPA[N-1:4]};
+                8'b0000_x101:RES<={OPA[4:0],OPA[N-1:5]};
+                8'b0000_x110:RES<={OPA[5:0],OPA[N-1:6]};
+                8'b0000_x111:RES<={OPA[6:0],OPA[N-1:7]};
                 default:;
               endcase
               ERR <= 1'b0;
@@ -414,13 +393,13 @@ parameter N=8;
           end
           default:
           begin
-            RES   <= 0;
-            COUT  <= 1'b0;
-            OFLOW <= 1'b0;
-            G     <= 1'b0;
-            E     <= 1'b0;
-            L     <= 1'b0;
-            ERR   <= 1'b0;
+            RES<=0;
+            COUT<=1'b0;
+            OFLOW<=1'b0;
+            G<=1'b0;
+            E<=1'b0;
+            L<=1'b0;
+            ERR<=1'b0;
           end
         endcase
       end
